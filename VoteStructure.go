642d3ce052e683d257111633eb9d6a6fc5b5c481
Ryan <rpatter5@asu.edu>
2020-08-17 11:07:52 -0700

    completed the template for the vote

--- VoteStructure.go
+++ VoteStructure.go
@@ -22,31 +22,138 @@ type Ballot struct {
 	ListOfRoles []GovernmentRole `json:"listofroles"`
 }
 
-
-func main() {
-
+func initializeBallot() *Ballot {
 	ballot := &Ballot{
-		BallotName: "blockchain vote test",
-		NumOfRoles: 1,
+                BallotName: "blockchain vote test",
+                NumOfRoles: 1,
 
-		ListOfRoles: []GovernmentRole{
-			GovernmentRole{
-				RoleName: "U.S. President",
+                ListOfRoles: []GovernmentRole{
+                        GovernmentRole{
+                                RoleName: "U.S. President",
+                                NumEligable: 1,
+
+                                ListOfCandidates: []Candidate{
+                                        Candidate {
+                                                CandidateName: "Ryan Patterson",
+                                                IsElected: false,
+                                        },
+					Candidate {
+						CandidateName: "Andrew Bradjan",
+						IsElected: false,
+					},
+					Candidate {
+						CandidateName: "Saiteja Mallineni",
+						IsElected: false,
+					},
+                                },
+                        },
+			GovernmentRole {
+				RoleName: "U.S. Senate",
 				NumEligable: 1,
 
-				ListOfCandidates: []Candidate{
+				ListOfCandidates: []Candidate {
 					Candidate {
-						CandidateName: "Ryan Patterson",
-						IsElected: true,
+						CandidateName: "Philip Bernick",
+						IsElected: false,
+					},
+					Candidate {
+						CandidateName: "Vishwam Annam",
+						IsElected: false,
 					},
 				},
 			},
-		},
-	}
-	fmt.Println(ballot.BallotName)
-	fmt.Println()
-	fmt.Println(ballot.ListOfRoles[0].NumEligable)
-	fmt.Println(ballot.ListOfRoles[0].ListOfCandidates[0].CandidateName)
+			GovernmentRole {
+				RoleName: "U.S. House of Representatives",
+				NumEligable: 2,
+
+				ListOfCandidates: []Candidate {
+					Candidate {
+						CandidateName: "Kiran Nagula",
+						IsElected: false,
+					},
+					Candidate {
+						CandidateName: "Rajeswari Gudiputi",
+						IsElected: false,
+					},
+					Candidate {
+						CandidateName: "Srija Vadem",
+						IsElected: false,
+					},
+				},
+
+
+			},
+			GovernmentRole {
+				RoleName: "State Senate",
+				NumEligable: 1,
+
+				ListOfCandidates: []Candidate {
+					Candidate {
+						CandidateName: "Tracie Gasik",
+						IsElected: false,
+					},
+					Candidate {
+						CandidateName: "Rhonda Steele",
+						IsElected: false,
+					},
+
+				},
+			},
+			GovernmentRole {
+				RoleName: "Court Attourney",
+				NumEligable: 1,
+
+				ListOfCandidates: []Candidate {
+					Candidate {
+						CandidateName: "Raj Katam",
+						IsElected: false,
+					},
+					Candidate {
+						CandidateName: "Sruthima Mantena",
+						IsElected: false,
+					},
+				},
+			},
+			GovernmentRole {
+				RoleName: "Court Sheriff",
+				NumEligable: 1,
+
+				ListOfCandidates: []Candidate {
+					Candidate {
+						CandidateName: "Aruna Ganta",
+						IsElected: false,
+					},
+					Candidate {
+						CandidateName: "Kristen Pond",
+						IsElected: false,
+					},
+				},
+			},
+			GovernmentRole {
+				RoleName: "County School Superindendent",
+				NumEligable: 1,
+
+				ListOfCandidates: []Candidate {
+					Candidate {
+						CandidateName: "Nagamani Kummari",
+						IsElected: false,
+					},
+					Candidate {
+						CandidateName: "Sena Reddy",
+						IsElected: false,
+					},
+				},
+			},
+                },
+        }
+
+	return ballot
+}
+
+
+func main() {
+
+	ballot := initializeBallot()
 
 	jsonstr, _ := json.Marshal(ballot)
 	fmt.Println(string(jsonstr))
