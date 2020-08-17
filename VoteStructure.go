09c97fc97a00d1d9991e84766283e225d4eed44d
Ryan <rpatter5@asu.edu>
2020-08-17 10:02:23 -0700

    committing code to move to another work station. Need to change variables from private to global variables to enable json converter

--- VoteStructure.go
+++ VoteStructure.go
@@ -2,57 +2,30 @@ package main
 
 import (
 	"fmt"
-	//"strings"
+	"encoding/json"
 )
 
 type Candidate struct {
-	candidateName string
-	isElected bool
+	candidateName string `json:"CandidateName"`
+	isElected     bool   `json:"IsElected"`
 }
 
 type GovernmentRole struct {
-	roleName string
-	numEligable int
-	listOfCandidates []Candidate
+	roleName         string      `json:"RoleName"`
+	numEligable      int         `json:"NumEligable"`
+	listOfCandidates []Candidate `json:"ListOfCandidates"`
 }
 
 type Ballot struct {
-	ballotName string
-	numOfRoles int
-	listOfRoles []GovernmentRole
+	ballotName  string           `json:"BallotName"`
+	numOfRoles  int              `json:"NumOfRoles"`
+	listOfRoles []GovernmentRole `json:"ListOfRoles"`
 }
 
 
 func main() {
-	var candidate = Candidate {
-		candidateName: "Ryan Patterson",
-		isElected: true,
-	}
 
-	fmt.Println(candidate.candidateName)
-	if (candidate.isElected) {
-		fmt.Println("elected")
-	}
-
-	var role = GovernmentRole {
-		roleName: "U.S. President",
-		numEligable: 1,
-		listOfCandidates: []Candidate {
-			Candidate {
-				candidateName: "Ryan Patterson",
-				isElected: true,
-			},
-		},
-	}
-
-	fmt.Println(role.roleName)
-	fmt.Println(role.numEligable)
-	fmt.Println(role.listOfCandidates[0].candidateName)
-
-
-
-
-	var ballot = Ballot{
+	ballot := &Ballot{
 		ballotName: "blockchain vote test",
 		numOfRoles: 1,
 
@@ -74,6 +47,9 @@ func main() {
 	fmt.Println()
 	fmt.Println(ballot.listOfRoles[0].numEligable)
 	fmt.Println(ballot.listOfRoles[0].listOfCandidates[0].candidateName)
+
+	jsonstr, _ := json.Marshal(ballot)
+	fmt.Println(string(jsonstr))
 
 }
 
